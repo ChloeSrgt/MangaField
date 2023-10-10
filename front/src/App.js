8247a79a1b42e12be9fb7b64d95b7bd390fa1535
ChloeSrgt <chloe.sirgent@gmail.com>
2023-10-10 14:40:57 +0200

    API display

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
+import LibraryMap from "./pages/libraryMap";
 import Login from "./pages/login";
+import MyProfil from "./pages/myProfil";
 import Register from "./pages/register";
-
+import UserLibrary from "./pages/userLibrary";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/libraryMap" element={<LibraryMap />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/myProfil" element={<MyProfil /> } />
         <Route path="/register" element={<Register />} />
+        <Route path="/userLibrary" element={<UserLibrary />} />
       </Routes>
     </Router>
   );
--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,70 +1,85 @@
-import { makeStyles } from '@mui/styles';
-import SearchBar from '../components/searchBar';
-import React from 'react';
+import { makeStyles } from "@mui/styles";
+import SearchBar from "../components/searchBar";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+    gap: "20px",
+    width: "80%",
+    marginTop: "20px",
+  },
+  mangaCard: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    border: "1px solid #ddd",
+    padding: "10px",
+    transition: "transform 0.3s",
+    "&:hover": {
+      transform: "scale(1.05)",
     },
-    grid: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
-        gap: '20px',
-        width: '80%',
-        marginTop: '20px'
-    },
-    mangaCard: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        border: '1px solid #ddd',
-        padding: '10px',
-        transition: 'transform 0.3s',
-        '&:hover': {
-            transform: 'scale(1.05)'
-        }
-    },
-    mangaImage: {
-        width: '100%',
-        height: 'auto',
-        borderRadius: '8px',
-        objectFit: 'cover'
-    },
-    mangaTitle: {
-      fontSize: '5px',
-      alignItems: 'center',
-    }
+  },
+  mangaImage: {
+    width: "100%",
+    height: "auto",
+    borderRadius: "8px",
+    objectFit: "cover",
+  },
+  mangaTitle: {
+    fontSize: "15px",
+    alignItems: "center",
+  },
 }));
 
 const Home = () => {
-    const classes = useStyles();
-    const mangas = [
-        {
-            title: 'Chainsaw Man',
-            imageURL: 'https://www.bdnet.com/img/couvpage/35/9782820338358_cg.jpg'
-        },
-        {
-            title: "l'Attaque des Titans",
-            imageURL: 'https://m.media-amazon.com/images/I/81InD3fqZAL._SL1500_.jpg'
-        }
-    ];
+  const classes = useStyles();
+  const [mangas, setMangas] = useState([]);
 
-    return (
-        <div className={classes.container}>
-            <SearchBar />
-            <h1>Catalogue Manga</h1>
-            <div className={classes.grid}>
-                {mangas.map((manga, index) => (
-                    <div key={index} className={classes.mangaCard}>
-                        <img src={manga.imageURL} alt={manga.title} className={classes.mangaImage} />
-                        <h2 className={classes.mangaTitle}>{manga.title}</h2>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/manga")
+      .then((response) => {
+        setMangas(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des mangas: ", error);
+      });
+  }, []);
+
+  return (
+    <div className={classes.container}>
+      <SearchBar />
+      <h1>Catalogue Manga</h1>
+      <div className={classes.grid}>
+        {mangas.map((manga, index) => (
+          <div key={manga.id} className={classes.mangaCard}>
+            <img
+              src={manga.image}
+              alt={manga.title}
+              className={classes.mangaImage}
+            />
+            <h2 className={classes.mangaTitle}>{manga.title}</h2>
+            <p>{manga.description}</p>
+            <p>
+              <strong>Auteur :</strong> {manga.author}
+            </p>
+            <p>
+              <strong>Date de sortie :</strong>{" "}
+              {new Date(manga.releaseDate).toLocaleDateString()}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
